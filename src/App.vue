da3113d814cc27e4d46ebce787909ed495a44f3f
Oleksandr58 <kovaliukoleksandr@gmail.com>
2021-07-05 22:51:23 +0300

    Add dynamic component with connfig

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <select v-model="select">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+    </select>
+
+    <!-- <div>
+      {{ currentTabComponent.type }}
+    </div> -->
+
+    <component v-if="currentTabComponent" :is="currentTabComponent" class="tab"></component>
+  </div>
+</template>
+
+<script>
+import { configs } from './config';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      select: '',
+    };
+  },
+  computed: {
+      currentTabComponent() {
+        const type = `type${this.select}`;
+        const selectedConfig = configs.find(config => config.type === type);
+
+        return selectedConfig?.component;
+      },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/config.js
@@ -0,0 +1,18 @@
+import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld1 from './components/HelloWorld1.vue';
+import HelloWorld2 from './components/HelloWorld2.vue';
+
+export const configs = [
+    {
+        type: 'type1',
+        component: HelloWorld,
+    },
+    {
+        type: 'type2',
+        component: HelloWorld1,
+    },
+    {
+        type: 'type3',
+        component: HelloWorld2,
+    },
+];
